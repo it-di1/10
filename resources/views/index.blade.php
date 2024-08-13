19a07dc2709fda1c8c1890fb92c4e4bd69cc884c
Dana <421202067@qu.edu.sa>
2024-08-13 15:41:13 +0300

    1st

---
+++ resources/views/index.blade.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>بانوراما القصيم للبرمجة والتصميم</title>
+    <link rel="stylesheet" href="assets\css\main.css">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+	<div id="main-page">
+    <div class="container">
+        <div class="project-name">
+            <h1>بانوراما القصيم للبرمجة والتصميم</h1>
+            <img src="images/logo.png" alt="Logo" >
+        </div>
+        <nav class="navbar">
+            <ul>
+				<li><a href="#contact" onclick="showContent('contact')">تواصل معنا</a></li>
+                <li><a href="#work" onclick="showContent('work')">عملائنا </a></li>
+                <li><a href="#services" onclick="showContent('about')">خدماتنا</a></li>
+                <li><a href="#about" onclick="showContent('services')">من نحن</a></li>
+            </ul>
+        </nav>
+</div>
+
+		
+		<article id="about">
+			<div class="glass-box">
+				<h2 class="major">من نحن</h2>
+				<span class="image main"><img src="images/pic03.jpg" alt="من نحن" /></span>
+				<p>
+					إيمانًا منا بأهمية البرمجة و العالم الرقمي في حياتنا كنا نحن بانوراما القصيم، من الرواد في هذه المجالاتي التي تسعى للارتقاء بمجال التصميم والبرمجة لتكون من كبرى الصروح بالمملكة والشرق الأوسط في تصميم وبرمجة المواقع وتطبيقات الجوال والبرامج الإدارية والمحاسبية .
+				</p>
+		<p>
+			نهدف دائمًا لإرضاء كافة العملاء لدينا من خلال تقديم مجموعة من الخدمات المميزة. نسعى لخلق وتنمية جسور التعاون والشراكة وتبادل الخبرات بين المبرمجين في مختلف تخصصاتهم، ونستهدف إعداد برامج متميزة تلبي الاحتياجات الحقيقية للمجتمع وسوق العمل مع تطويرها المستمر. ندرب المبرمجين حديثي التخرج لنرتقي بـالمستويات المهارية في جميع مجالات البرمجة من الأنشطة الأساسية لخدمة المجتمع المدني.
+		</p>
+		<p>
+			رسالتنا هي خلق مجتمع واعِ فكريًا و متطور علميًا، قادرًا على مواكبة العالم الرقمي بأسهل الطرق وأبسطها.
+		</p>
+			</div>
+		</article>
+        
+		<article id="work">
+			<h2 class="major"> عملائنا</h2>
+			<div class="glass-container">
+
+				<div class="partner-item">
+				<span class="image main"><img src="images/pic02_3.jpg" alt="مستشفى بريدة المركزي"/></span>
+					<h3>مستشفى بريدة المركزي </h3>
+					<p class="arabic-text">
+						صرح طبي عريق بالقصيم منطقة بريده
+						أنجز مستشفى بريدة 
+						المركزي أحد مكونات تجمع القصيم الصحي , أكثر من 12 ألف عملية جراحية حتى نهاية الربع
+						الثالث " يناير- سبتمبر" خلال 9 أشهر ، من العام الحالي 2023م ، توزعت على مختلف 
+						الأقسام بالمستشفى
+					</p>
+			   </div>
+
+				<div class="partner-item">
+					  <span class="image main"><img src="images/pic02.jpg" alt="اركال للاستشارات الهندسية" /></span>
+						 <h3>اركال للاستشارات الهندسية</h3>
+							<p>أركال للإستشارات الهندسيه المتخصص في تصميم المخططات المعماريه بأحدث الأساليب العصريه وتصميم المخططات الإنشائيه تماشيا مع الأكواد العالميه بالإضافة لجميع الأعمال المساحيه والكروكيات التنظيميه
+								 وأعمال مكافحة الحريق والأمن والسلامه.</p><br>
+				</div>
+				
+				<div class="partner-item">
+					<span class="image main"><img src="images/pic02_1.jpg" alt="" /></span>
+					   <h3>نادي الرائد الرياضي</h3>
+						 <p>
+						رؤيتنا أن نكون ناديًا
+						 رائدًا يعزز النمو المستدام في المجال الرياضي محلياً ودولياً
+						 ورسالتنا توفير بيئة رياضية محفّزة في مختلف الألعاب الرياضية،
+						 وخلق بيئة جاذبة للاستثمار، و تفعيل المسؤولية الاجتماعية، 
+						وتعزيز الإستدامة المهنية والمالية وذلك لتحقيق تطلعات
+						 الشركاء والتوجهات المستقبلية للمملكة
+						</p>
+				  <br>
+				</div>
+
+				<div class="partner-item">
+				   <span class="image main"><img src="images/pic02_4.jpg" alt="مخابز وحلويات الأرياف" /></span>
+					 <h3>مخابز وحلويات الأرياف</h3>
+						 <p>مخابز وحلويات الأرياف.. 40 عاماً من التميز والنجاح 11 فرعاً حول المملكة تقدم أفضل المنتجات
+						 <br>
+					  بدأت قصة شركة مخابر وحلويات الأرياف عام
+					   1985 ميلادي على يد المؤسس رجل الأعمال فهد المحيميد،
+					   من حلم إلى كيان عريق يملك خبرة ممتدة ومحفوفة
+					   بالعراقة والأصالة، كانت هذه العلامة التجارية
+					   شريكة في الكثير من اللحظات السعيدة للمجتمع
+					   مما جعل لهذه العلامة ولاء عاليا من قبل عملائها،
+					   حتى أصبحت لا تكتمل الأفراح لسنوات طويلة إلا بصحبة
+					   منتجات مخابز وحلويات الأرياف، وكان لدى المؤسس 
+					  الأستاذ فهد المحيميد بعد نظر في استثماره،
+					   حيث حرص على أن لا يخدم قطاعاً واحداً فقط،
+					   بل قدم منتجات نوعية في قطاعات متعددة مثل:
+					<br>*
+					الحلويات الشرقية من الكنافة والبقلاوة وغيرها.
+					 <br>*
+					 الحلويات الغربية من الكيكات الفاخرة، والكثير من الوصفات المبتكرة
+					  والمستوحاة من المطبخ الأوروبي وغيرها.
+					 <br>*الشوكولاتة الفاخرة بأعلى مستوى وألذ حشوة.
+					 <br>*المعجنات والمقليات الطازجة، والمخبوزات التي هي خيار دائم لأصحاب المنازل.
+				</p>
+				   <br>
+		 </div>
+			
+		 <div class="partner-item">
+			<span class="image main"><img src="images/pic02_5.jpg" alt="النادي العربي" /></span>
+			<h3> النادي العربي</h3>
+			<p>
+				النادي العربي الرياضي بعنيزة, في محافظة عنيزة بمنطقة القصيم وسط المملكة العربية السعودية تأسس سنة 1958, ويحمل النادي شعارا باللون الأحمر والأبيض. يُلقب النادي العربي بفارس عنيزة، وهو نادي رياضي يضم العديد من الألعاب الرياضية أبرزها كرة القدم وكرة اليد وكرة السلة. ويقع مقر النادي في محافظة عنيزة بمنطقة القصيم
+			</p>
+			<br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_7.jpg" alt="نادي الصقر " /></span>
+			<h3> نادي الصقر </h3>
+			<p>
+				تاسس نادي الصقر عام 1401هـ على يد مجموعة من اهالي مركز البصر وحينها تم ترشيح الاستاذ عبد العزيز بن عبد الرحمن السويد كاول رئيس للنادي,
+				 ثم تعاقب على ادارة النادي احد عشر رئيسا كان اخرهم الرئيس الحالي الاستاذ فهد بن عبد العزيز المحيميد.حقق النادي اول بطولاته على مستوى منطقة القصيم
+				 العام 1432هـ وفي العام 1442هـ حقق النادي ذات البطولة للمرة الثانية .
+				 وفي نفس العام حقق نادي الصقر تاج بطولاتة : درع
+				 المملكة العربية السعودية الاندية الدرجة الثالثة, بقيادة رئيس النادي
+				 الاستاذ فهد بن عبدالعزيز المحيميد, وعلى اثرها تأهل لاول
+				 مرة في تاريخه لدوري الدرجة الثانية لكرة القدم .
+			</p><br>        
+		</div>
+		   
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_8.jpg" alt="نادي السر " /></span>
+			<h3>نادي السر </h3>
+			<p>
+			 نسعى بنادي السر الرياضي للتميز الرياضي والإداري عبر عمل مؤسسي لتحقيق أهدافنا
+			  وطموحاتنا التي تلبي تطلعات أصحاب المصلحة.
+			<br>وأن نكون متميزين في مختلف المجالات الرياضية ونموذجاً في التطوير المؤسسي<br>
+			التميز روح الفريق الاستدامة الشراكة الشفافية التمكين
+		   <br>ولتحقيق التميز المؤسسي تحسين المنافسة الرياضية تعزيز الاستدامة المالية زيادة
+			مشجعي النادي تحسين البنية تفعيل التكامل و الشراكات المجتمعية</p><br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_6.jpg" alt="نادي التقدم" /></span>
+			<h3>نادي التقدم</h3>
+			<p>
+		كانت بدايات النادي بسيطة باجتهادات ألهالي المذنب في عام 1968م، حيث تولى رئاسته إدارته بداح البداح - رحمه هللا تعالى -حتى جاءت الموافقة الكريمة على اعتماد نادي التقدم الرياضي في محافظة المذنب لممارسة كافة أنشطته الرياضية والثقافية واالجتماعية عام 1972 م ويتولى رئاسة مجلس إدارته عثمان بن محمد الدخيل - رحمه الله تعالى - وتتعاقب بعده اإلدارات لوقتنا الحالي.
+			</p><br>
+		</div>
+
+		<div class="partner-item">
+			 <span class="image main"><img src="images/pic02_9.jpg" alt="حاضنة الجمعيات" /></span>
+			<h3>حاضنة الجمعيات</h3>
+			<p>
+			 حاضنة للجمعيات الأهلية الناشئة هدفها دراسة الاحتياجات بمنطقة القصيم ودعم الافكار ورعايتها 
+			  ودعم الجمعيات من خلال سبل الاستدامة المالية وتنفيذ افكارها،
+			 وحل مشكلة الصعوبات والعوائق التي تواجهها، وكيفية حلها ،
+			   ومعرفة الاحتياجات بهدف مساعدتها لتأسيس هذه الجمعية.
+			</p><br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_10.jpg" alt="كليات عنيزة" /></span>
+			<h3>كليات عنيزة</h3>
+			<p> 
+			  تعد كليات عنيزة من الكليات الناشئة في المملكة؛
+			  إذ انطلقت مسيرتها في العام الجامعي 1437/1436 ه بدعم سخي من حكومة خادم الحرمين الشريفين الملك سلمان بن عبدالعزيز آل سعود،
+			   وسمو ولي عهده الأمين، حفظهما الله، ورعاية كريمة من وزارة التعليم،
+			   تحقيقا للتوجهات المستقبلية للمملكة في مجال التعليم، والبحث العلمي؛ 
+			   لتوفير تعليم متميز لأبناء المملكة وبناتها في منطقة القصيم
+			</p><br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_11.jpg" alt="جمعية تجهيز" /></span>
+			<h3>جمعية تجهيز</h3>
+			<p> 
+			 جمعية خيرية تهتم بشؤون المسلم بعد موته، وتوعية المجتمع بأحكام الجنائز من خلال تقديم
+			 عمل خيري متقن للمجتمع من خلال الاهتمام برعاية شؤون الموتى، تجهيزا، وتكفينا، ودفنا،
+			 وتدريب وتأهيل المغسلين، وتوعية المجتمع بالمفهوم الشرعي للتعامل مع الجنائز.
+			</p><br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_12.jpg" alt="مكتب المحامي عبدالرحمن الجمعة" /></span>
+			<h3> مكتب المحامي عبدالرحمن الجمعة </h3>
+			<p> 
+			 مكتب عبدالرحمن محمد الجمعة محامون ومستشارون وموثقون هو مكتب مرخص له بمزاولة 
+			  المحاماة بموجب ترخيص المحاماة
+			 رقم 39389 وتاريخ 1/7/1439 وترخيص التوثيق رقم 1767 /40 وتاريخ 1/9/1440هـ 
+			  الصادرين من وزارة العدل بالمملكة العربية السعودية
+			</p><br>
+		</div>
+
+		<div class="partner-item">
+			<span class="image main"><img src="images/pic02_13.jpg" alt="مكتب القصيم للزواج" /></span>
+			<h3>مكتب القصيم للزواج</h3>
+			<p> 
+				 مكتب القصيم للزواج القصيم بريده حي السالميه شرق مسجد الجارالله الجامع
+				المكتب له حوالي 16سنه زوج كثير من الرجال
+				 على مستوى المملكه دخولك وتعرفك بالمكتب انت بالطريق الصحيح وانتبه لنفسك من
+				 المتطفلين على الزواج في برامج التواصل الذينا ليسو مختصين بالزواج وإنما هدفهم
+				اخذ أموال الناس بالباطل المكتب معروف لدى الجميع ومنهم الجهات المختصه .
+			</p><br>
+		</div>
+   </div>
+		</article>
+    
+		
+			<section id="services">
+				<div class="glass-container">
+					<div class="service-item">
+						<h2 class="major">البرمجة من الصفر</h2>
+						<img src="images/pic01_7.jpg" alt="Service 1">
+						<p class="arabic-text">
+							نقوم بتحويل فكرتك إلى موقع إلكتروني او تطبيق جوال مع سكريبت مبرمج من الصفر.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">سكريبت الجمعيات الخيرية</h2>
+						<img src="images/pic01_6.jpg" alt="Service 2">
+						<p class="arabic-text">
+							سكريبت متكامل لمواقع الجمعيات الخيريه وفق ضوابط المركز السعودي للحوكمة.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">برنامج قمة للنداء الآلي</h2>
+						<img src="images/pic01.jpg" alt="Service 3">
+						<p class="arabic-text">
+							هو نظام تبليغ عن الحالات الطارئه للمنشآت الصحية أو غيرها من الأماكن التي تحتاج سرعة إستجابة.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">برنامج قمة لنقاط البيع</h2>
+						<img src="images/pic01_1.jpg" alt="Service 4">
+						<p class="arabic-text">
+							* لا يحتاج جــــــهاز بمواصفات عاليه.<br>
+							* يعمل على الجوال.<br>
+							* قاعدة بيانات محمية مرفوعة على الشبكة.<br>
+							* نسخ احتياطي كل 24 ساعه.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">منصة بانوراما المتكاملة للاندية الرياضيه</h2>
+						<img src="images/pic01_5.jpg" alt="Service 5">
+						<p class="arabic-text">
+							تقدم بانوراما القصيم للبرمجة منصة متكاملة لادارة الانديه الرياضيه تشتمل على الآتي: <br>
+							* موقع الكتروني.<br>
+							* تطبيقين جوال للموقع ايفون و اندرويد.<br>
+							* متجر الكتروني.<br>
+							* نظام المراسلات الالكترونيه.<br>
+							* منصة تذاكر.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">نظام قمة للمحاسبة</h2>
+						<img src="images/pic01_2.jpg" alt="Service 6">
+						<p class="arabic-text">
+							هو نظام يمكن مكاتب المحاسبه من ادارة جميع اعمالها مثل: <br>
+							* الحسابات.<br>
+							* القيود اليوميه.<br>
+							* ميزان المراجعه.<br>
+							* القوائم الماليه.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">نظام قمة للمحاماه</h2>
+						<img src="images/pic01_3.jpg" alt="Service 7">
+						<p class="arabic-text">
+							هو نظام يمكن مكاتب المحاماه من ادارة جميع اعمالها مثل: <br>
+							* تقديم الدعاوي.<br>
+							* ادارة المحاميين و توكيل القضايا لهم.<br>
+							* خط سير لمتابعة كل قضيه.
+						</p>
+					</div>
+					<div class="service-item">
+						<h2 class="major">منصة بانوراما لإدارة المشاريع الهندسية</h2>
+						<img src="images/pic01_4.jpg" alt="Service 8">
+						<p class="arabic-text">
+							تقدم بانوراما القصيم للبرمجه منصة متكاملة لإدارة المكاتب الهندسيه الكبيرة، تشتمل على: <br>
+							* خط سير كامل للمشاريع الهندسية بكامل تفاصيلها.<br>
+							* ادارة الموظفين و اجازاتهم و رواتبهم واقاماتهم.<br>
+							* نظام اشعارات مخصص لكل بند من البنود اعلاه.<br>
+							* ادارة السجلات و تراخيص المحلات.<br>
+							* ادارة الموادر البشريه.<br>
+							* ادارة متكامله لسيارات المكتب الهندسي.
+						</p>
+					</div>
+				</div>
+			</section>
+			
+
+
+    <div class="container">
+		
+	
+		<article id="contact" class="glass-effect">
+			<h2 class="major">اترك لنا رسالتك</h2>
+			<form method="post" action="#">
+				<div class="fields">
+					<div class="field half">
+						<label for="name">الاسم</label>
+						<input type="text" name="name" id="name" />
+					</div>
+					<div class="field half">
+						<label for="email">البريد الالكتروني</label>
+						<input type="text" name="email" id="email" />
+					</div>
+					<div class="field">
+						<label for="message">الرسالة</label>
+						<textarea name="message" id="message" rows="4"></textarea>
+					</div>
+				</div>
+				<ul class="actions">
+					<li><input type="submit" value="ارسال" class="primary" /></li>
+					<li><input type="reset" value="مسح" /></li>
+				</ul>
+			</form>
+			<div class="social">
+				<a href="https://x.com/Panorama_Q" target="_blank"><i class='bx bxl-twitter'></i></a>
+			    <a href="https://facebook.com/palqassim" target="_blank"><i class='bx bxl-facebook-square' ></i></a>
+				<a href="https://api.whatsapp.com/send/?phone=9660553172552" target="_blank"><i class='bx bxl-whatsapp' ></i></a>
+				<a href="https://snapchat.com/t/mh05HGb6" target="_blank"><i class='bx bxl-snapchat' ></i></a>
+				<a href="https://tiktok.com/@panorama_alqassim" target="_blank"><i class='bx bxl-tiktok'></i> </a>
+				<a href="https://instagram.com/palqassim/" target="_blank"> <i class='bx bxl-instagram'></i></a>
+			</div>
+		</article>
+	
+	</div>
+
+
+
+<!--	<article id="contact">
+		<h2 class="major">اترك لنا رسالتك</h2>
+		<form method="post" action="#">
+			<div class="fields">
+				<div class="field half">
+					<label for="name">الاسم</label>
+					<input type="text" name="name" id="name" />
+				</div>
+				<div class="field half">
+					<label for="email">البريد الالكتروني</label>
+					<input type="text" name="email" id="email" />
+				</div>
+				<div class="field">
+					<label for="message">الرسالة</label>
+					<textarea name="message" id="message" rows="4"></textarea>
+				</div>
+			</div>
+			<ul class="actions">
+				<li><input type="submit" value="ارسال" class="primary" /></li>
+				<li><input type="reset" value="مسح" /></li>
+			</ul>
+		</form> -->
+
+
+		<a href="#main-page" class="back-to-home">
+			<img src="images/webpagehome_85808.ico" alt="Home">
+		</a>
+	</article>
+	
+	<style>
+		.back-to-home {
+			position: fixed;
+			top: 20px;
+			right: 20px;
+			width: 60px;
+			height: 60px;
+			background: rgba(255, 165, 0, 0.3); 
+			backdrop-filter: blur(10px);
+			border-radius: 50%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+			transition:  0.3s ease, transform 0.3s ease;
+			z-index: 1000;
+		}
+	
+		.back-to-home img {
+			width: 30px;
+			height: 30px;
+		}
+	
+		.back-to-home:hover {
+			background: rgba(255, 165, 0, 0.6);
+			transform: scale(1.1);
+		}
+	</style>
+
+
+    <script>
+		const services = document.querySelectorAll('.service');
+
+		services.forEach(service => {
+			const title = service.querySelector('.service-title');
+			title.addEventListener('click', () => {
+				const content = service.querySelector('.service-content');
+				content.classList.toggle('active');
+			});
+		});
+    </script>
+</body>
+</html>
