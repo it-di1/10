11cdb7fa929067a61c353d388f6c8bcd7ab4a51f
Dana <421202067@qu.edu.sa>
2024-08-15 18:13:30 +0300

    i

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -5,7 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body{
+            background:#220000;
+        }
+        #contact {
+            width: 600px; 
+            background:white;
+            margin: 0 auto; 
+            margin-top:100px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
 
+    
+        #contact h2 {
+            text-align: center;
+        }
+
+        #contact .field {
+            display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;}
+
+        #contact .actions {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     
@@ -15,22 +43,21 @@
                 @csrf
                 <div class="fields">
                     <div class="field half">
-                        <label for="name">الاسم</label>
-                        <input type="text" name="name" id="name" />
+                        <label for="name"></label>
+                        <input type="text" name="name" id="name" placeholder="الاسم" />
                     </div>
                     <div class="field half">
-                        <label for="email">البريد الالكتروني</label>
-                        <input type="text" name="email" id="email" />
+                        <label for="email"></label>
+                        <input type="text" name="email" id="email"placeholder="البريد الإلكتروني" />
                     </div>
                     <div class="field">
-                        <label for="message">الرسالة</label>
-                        <textarea name="message" id="message" rows="4"></textarea>
+                        <label for="message"></label>
+                        <textarea name="message" placeholder="الرسالة" id="message" rows="4"></textarea>
                     </div>
                 </div>
                 <ul class="actions">
-                    <li><input type="submit" value="ارسال" class="primary" /></li>
-                    <li><input type="reset" value="مسح" /></li>
-                </ul>
+                    <input type="submit" value="ارسال" class="primary" />
+                    <input type="reset" value="مسح" />
             </form> 
             
         </article> 
