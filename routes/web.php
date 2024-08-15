686e7de5a806029777a2c8696a1ae9eedbcdbcc2
lujai <412205544@qu.edu.sa>
2024-08-15 17:12:50 +0300

    h

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::get('/data', function () {
+Route::get('/', function () {
    return view('index');
 });
 
