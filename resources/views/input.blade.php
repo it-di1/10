9c74bcf9b2a9009b77767a3b0c85be28b0bc0fe1
lujai <412205544@qu.edu.sa>
2024-08-15 13:44:18 +0300

    h

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -9,6 +9,32 @@
 </head>
 <body>
     
+<article id="contact" class="glass-effect">
+            <h2 class="major">اترك لنا رسالتك</h2>
+            <form method="post" action="#">
+                <div class="fields">
+                    <div class="field half">
+                        <label for="name">الاسم</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div class="field half">
+                        <label for="email">البريد الالكتروني</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div class="field">
+                        <label for="message">الرسالة</label>
+                        <textarea name="message" id="message" rows="4"></textarea>
+                    </div>
+                </div>
+                <ul class="actions">
+                    <li><input type="submit" value="ارسال" class="primary" /></li>
+                    <li><input type="reset" value="مسح" /></li>
+                </ul>
+            </form> 
+            
+        </article> 
+    
+
 
 
 
