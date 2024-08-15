9c3bf3b428ca4a22102a13b3da7e2cb5037bc3cc
lujai <412205544@qu.edu.sa>
2024-08-15 13:11:23 +0300

    h

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -10,17 +10,35 @@
 <body>
     
     
-<form>
-  <div class="p-3 m-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+<div class="container">
+    
+  
+    <article id="contact" class="glass-effect">
+      <h2 class="major">اترك لنا رسالتك</h2>
+      <form method="post" action="#">
+        <div class="fields">
+          <div class="field half">
+            <label for="name">الاسم</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div class="field half">
+            <label for="email">البريد الالكتروني</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div class="field">
+            <label for="message">الرسالة</label>
+            <textarea name="message" id="message" rows="4"></textarea>
+          </div>
+        </div>
+        <ul class="actions">
+          <li><input type="submit" value="ارسال" class="primary" /></li>
+          <li><input type="reset" value="مسح" /></li>
+        </ul>
+      </form> 
+      
+    </article>
+  
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="text" class="form-control" id="exampleInputPassword1">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
 </body> 
 </html>
