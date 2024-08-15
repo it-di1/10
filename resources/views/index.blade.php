41c674ff22bff2d95b896d7f2cbbeed643b5b65c
Reema Alkhozaim <421202223@qu.edu.sa>
2024-08-15 07:00:28 +0300

    co

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -328,30 +328,6 @@
 
 
 
-<!--	<article id="contact">
-		<h2 class="major">اترك لنا رسالتك</h2>
-		<form method="post" action="#">
-			<div class="fields">
-				<div class="field half">
-					<label for="name">الاسم</label>
-					<input type="text" name="name" id="name" />
-				</div>
-				<div class="field half">
-					<label for="email">البريد الالكتروني</label>
-					<input type="text" name="email" id="email" />
-				</div>
-				<div class="field">
-					<label for="message">الرسالة</label>
-					<textarea name="message" id="message" rows="4"></textarea>
-				</div>
-			</div>
-			<ul class="actions">
-				<li><input type="submit" value="ارسال" class="primary" /></li>
-				<li><input type="reset" value="مسح" /></li>
-			</ul>
-		</form> -->
-
-
 		<a href="#main-page" class="back-to-home">
 			<img src="images/webpagehome_85808.ico" alt="Home">
 		</a>
