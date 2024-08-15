e59b9890d67737193fe962b7f847bcf6acecf4fe
lujai <412205544@qu.edu.sa>
2024-08-15 16:33:12 +0300

    h

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Route::get('/gg', function () {
 });
 
 
-//Route::get('/data', function () {
- // return view('input');
-//});
+Route::get('/data', function () {
+  return view('input');
+});
 
  Route::post('/hj', [ContactController::class,'data']);
 
