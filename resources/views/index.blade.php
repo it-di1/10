8103b088b47bba71118edbe50a06c9f42d75a29b
lujai <412205544@qu.edu.sa>
2024-08-17 14:08:52 +0300

    n

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <title>بانوراما القصيم للبرمجة والتصميم</title>
-    <!-- <link rel="stylesheet" href="assets\css\main.css"> -->
-	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+     <link rel="stylesheet" href="assets\css\main.css"> 
+	
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
