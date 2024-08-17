813c060a277f431fb4505590e0bde37fd2ec616e
lujai <412205544@qu.edu.sa>
2024-08-17 13:58:13 +0300

    n

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,7 @@
 
         <title>Laravel</title>
 
-<<<<<<< HEAD
         <!-- fonts -->
-=======
-        
->>>>>>> d3764bda2ce1053dcae1f24707126f411075b240
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
